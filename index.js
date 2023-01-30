54cdbc1542fa35199d53f41dd4150fc617170c07
Malcom Charles <61559987+Malcom-Charlie@users.noreply.github.com>
2023-01-30 23:27:57 +0300

    Add Js to Extbtn in modal

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const menu = document.getElementById('burger-menu');
 const modal = document.getElementById('modal');
 const body = document.querySelector('body');
+const exitBtn = document.getElementById('exit-button');
+const modalLnks = document.querySelectorAll('.modal__navigation__li');
 
 menu.addEventListener('click', (event) => {
     event.stopPropagation();
@@ -9,4 +11,14 @@ menu.addEventListener('click', (event) => {
     body.classList.add('stop-scrolling');
 });
 
+exitBtn.addEventListener('click', (event) => {
+    event.stopPropagation();
+    modal.classList.add('display-none');
+    body.classList.remove('stop-scrolling');
+});
 
+modalLnks.addEventListener('click', (event) => {
+    event.stopPropagation();
+    modal.classList.add('display-none');
+    body.classList.remove('stop-scrolling');
+});
