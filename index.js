31284e6e40a33388b51a5f481f83a4db95044fd9
David Espino <davidespinog@gmail.com>
2023-01-30 14:10:36 -0600

    Add JS functionality to burger menu

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const exitButton = document.getElementById('exit-button');
+const menu = document.getElementById('burger-menu');
+const modal = document.getElementById('modal');
+const body = document.querySelector('body');
+
+menu.addEventListener('click', (event) => {
+    event.stopPropagation();
+    console.log('it works');
+    modal.classList.remove('display-none');
+    body.classList.add('stop-scrolling');
+});
 
 
