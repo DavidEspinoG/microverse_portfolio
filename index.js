3395b48487362ededbbcc8fe6862bbd575141a4a
David Espino <davidespinog@gmail.com>
2023-01-30 15:38:28 -0600

    Fix linter errors

--- index.js
+++ index.js
@@ -3,26 +3,23 @@ const modal = document.getElementById('modal');
 const body = document.querySelector('body');
 const exitBtn = document.getElementById('exit-button');
 const modalLnks = document.querySelectorAll('.modal__navigation__li');
-console.log(modalLnks);
+
 menu.addEventListener('click', (event) => {
-    event.stopPropagation();
-    console.log('it works');
-    modal.classList.remove('display-none');
-    body.classList.add('stop-scrolling');
+  event.stopPropagation();
+  modal.classList.remove('display-none');
+  body.classList.add('stop-scrolling');
 });
 
 exitBtn.addEventListener('click', (event) => {
+  event.stopPropagation();
+  modal.classList.add('display-none');
+  body.classList.remove('stop-scrolling');
+});
+
+for (let i = 0; i < modalLnks.length; i += 1) {
+  modalLnks[i].addEventListener('click', (event) => {
     event.stopPropagation();
     modal.classList.add('display-none');
     body.classList.remove('stop-scrolling');
-});
-
-
-
-for (let i = 0; i < modalLnks.length;i ++){
-    modalLnks[i].addEventListener('click', (event) => {
-        event.stopPropagation();
-        modal.classList.add('display-none');
-        body.classList.remove('stop-scrolling');
-        });
+  });
 }
